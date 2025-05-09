3b5d986c94096c6ed92bb0c85754ac3c24ac76de
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2025-05-09 09:43:15 -0300

    linter

--- modern.config.ts
+++ modern.config.ts
@@ -1,6 +1,6 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
-import statePlugin from "@modern-js/plugin-state";
-import {tailwindcssPlugin} from "@modern-js/plugin-tailwindcss";
+import statePlugin from '@modern-js/plugin-state';
+import { tailwindcssPlugin } from '@modern-js/plugin-tailwindcss';
 
 export default defineConfig({
   runtime: {

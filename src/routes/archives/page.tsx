ca31a2e66a0876c01466709474a16c5809c6d0f4
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2025-05-09 10:05:57 -0300

    add navigation and navbar using radiochecks

--- src/routes/archives/page.tsx
+++ src/routes/archives/page.tsx
@@ -1,7 +1,6 @@
-import { List } from 'antd';
-
 import { Helmet } from '@modern-js/runtime/head';
-import Item from '../components/Item';
+import { List } from 'antd';
+import Item from '../../components/Item';
 
 const getAvatar = (users: Array<{ name: string; email: string }>) =>
   users.map(user => ({
@@ -9,21 +8,19 @@ const getAvatar = (users: Array<{ name: string; email: string }>) =>
     avatar: `https://api.dicebear.com/7.x/pixel-art/svg?seed=${user.name}`,
   }));
 
-const mockData = getAvatar([
-  { name: 'Thomas', email: 'w.kccip@bllmfbgv.dm' },
-  { name: 'Chow', email: 'f.lfqljnlk@ywoefljhc.af' },
-  { name: 'Bradley', email: 'd.wfovsqyo@gpkcjwjgb.fr' },
-  { name: 'Davis', email: '"t.kqkoj@utlkwnpwk.nu' },
-]);
-
+const getMockArchivedData = () =>
+  getAvatar([
+    { name: 'Thomas', email: 'w.kccip@bllmfbgv.dm' },
+    { name: 'Chow', email: 'f.lfqljnlk@ywoefljhc.af' },
+  ]);
 function Index() {
   return (
     <div className="container lg mx-auto">
       <Helmet>
-        <title>All</title>
+        <title>Archives</title>
       </Helmet>
       <List
-        dataSource={mockData}
+        dataSource={getMockArchivedData()}
         renderItem={info => <Item key={info.name} info={info} />}
       />
     </div>
